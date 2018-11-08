eb8b527d2d5f80a21d4ccd09455b70a9305f1da9
joddude <joddude@gmail.com>
2018-11-08 16:04:01 +0200

    Fix half-map bug - rework for new google map api.

--- world10/js/functions.js
+++ world10/js/functions.js
@@ -1,14 +1,9 @@
-function Coord() {
-  this.x;
-  this.y;
-}
-
+var COORDSPP = 5.12;
 
 function Coord(x,y) {
   this.x = x;
   this.y = y;
 }
-
 
 Coord.prototype.toString = function() {
   return "("+this.x+", "+this.y+")";
@@ -17,9 +12,39 @@ Coord.prototype.toString = function() {
 
 function myProjection() {
   var MAP_RANGE = 51200;
-  this.worldOrigin_ = this.fromCoordToPoint(new Coord(gup('x','0'),gup('y','0')));
-  this.pixelsPerLonDegree_ = MAP_RANGE / 360;
-  this.pixelsPerLatDegree_ = MAP_RANGE / 180;
+  this.worldOrigin_ = this.fromCoordToPoint(new Coord(0, 0));
+  this.pixelsPerLonDegree = MAP_RANGE / 360;
+  this.pixelsPerLatDegree = MAP_RANGE / 180;
+};
+
+myProjection.prototype.fromPointToCoord = function(point) {
+  return new Coord(COORDSPP * point.x, COORDSPP * point.y);
+};
+
+myProjection.prototype.fromCoordToPoint = function(coord) {
+  return new google.maps.Point(coord.x / COORDSPP, coord.y / COORDSPP);
+};
+
+myProjection.prototype.fromLatLngToCoord = function(latLng) {
+  return this.fromPointToCoord(this.fromLatLngToPoint(latLng));
+};
+
+myProjection.prototype.fromCoordToLatLng = function(coord) {
+  return this.fromPointToLatLng(this.fromCoordToPoint(coord));
+};
+
+myProjection.prototype.fromLatLngToPoint = function(latLng) {
+  var origin = this.worldOrigin_;
+  var x = origin.x + latLng.lng() * this.pixelsPerLonDegree;
+  var y = origin.y + latLng.lat() * this.pixelsPerLatDegree;
+  return new google.maps.Point(x, y);
+};
+
+myProjection.prototype.fromPointToLatLng = function(point) {
+  var origin = this.worldOrigin_;
+  var lng = (point.x - origin.x) / this.pixelsPerLonDegree;
+  var lat = (point.y - origin.y) / this.pixelsPerLatDegree;
+  return new google.maps.LatLng(lat, lng);
 };
 
 
@@ -39,9 +64,34 @@ function CoordMapType(tileSize) {
   this.tileSize = tileSize;
 }
 
+CoordMapType.prototype.getTile = function(coord, zoom, ownerDocument) {
+  coord = fixCoord(coord, zoom);
+  var factor = Math.pow(2, 9-zoom);
+  var x = ((coord.x)*factor);
+  var y = ((coord.y)*factor);
+  var div = ownerDocument.createElement('div');
+  div.innerHTML = '('+x+','+y+')';
+  div.style.width = this.tileSize.width + 'px';
+  div.style.height = this.tileSize.height + 'px';
+  div.style.fontSize = '10';
+  div.style.borderStyle = 'solid';
+  div.style.borderWidth = '1px';
+  div.style.borderColor = '#000000';
+  div.className = 'gridTile';
+  return div;
+};
+
+CoordMapType.prototype.toggle = function() {
+  if (map.overlayMapTypes.getLength()) {
+    map.overlayMapTypes.pop();
+  } else {
+    map.overlayMapTypes.push(new CoordMapType(new google.maps.Size(tsz[0], tsz[1])))
+  }
+};
+
 
 function url_update(){
-  coord = projection.fromLatLngToCoord(map.getCenter());
+  var coord = projection.fromLatLngToCoord(map.getCenter());
   location.hash = '#level='+map.getMapTypeId()+
     '&x='+coord.x.toFixed(2)+
     '&y='+coord.y.toFixed(2)+
@@ -53,4 +103,14 @@ function url_update(){
 
 function open_spreadsheet(){
   window.open('https://docs.google.com/spreadsheets/d/'+spreadsheet_id+'/edit', '_blank');
+}
+
+
+function fixCoord(coord, zoom) {
+  var x = coord.x;
+  var y = coord.y;
+  var factor = Math.pow(2, zoom) * 512;
+  if (x > factor/2)
+    x = x - factor;
+  return {x: x, y: y};
 }
--- world10/js/main.js
+++ world10/js/main.js
@@ -1,6 +1,5 @@
 var tsz = [100, 100]
 var gridActive = true;
-var COORDSPP = 5.12;
 
 //Change api key to YOUR_API_KEY in next line
 var api_key = 'AIzaSyBRubiQU_lzu8FjQTy-L5I2z57MwtJRAUg';
@@ -10,54 +9,10 @@ var oauth_client_id = '899386149897-u1cbun7uijmk5bnh8q81d6t5t1rreaah.apps.google
 var oauth_scopes = ['https://www.googleapis.com/auth/spreadsheets'];
 var default_tileset = 'tiles/';
 var default_spreadsheet_id = '1vQxLvqzQ8Z_N2ifbAni7Fu5NLEuT5tNR05Z_8bPQqHw';
-var tileset = gup('tileset',default_tileset);
+var tileset = gup('tileset', default_tileset);
 var spreadsheet_id = gup('spreadsheet', default_spreadsheet_id);
 var sheet_api_url = 'https://sheets.googleapis.com/v4/spreadsheets/' + spreadsheet_id;
 var site_url = location.protocol + '/' + location.host + location.pathname;
-
-var projection;
-myProjection.prototype.fromPointToCoord = function(point) {return new Coord(COORDSPP*point.x, COORDSPP*point.y)};
-myProjection.prototype.fromCoordToPoint = function(coord) {return new google.maps.Point(coord.x/COORDSPP, coord.y/COORDSPP);};
-myProjection.prototype.fromLatLngToCoord = function(latLng) {return this.fromPointToCoord(this.fromLatLngToPoint(latLng))};
-myProjection.prototype.fromCoordToLatLng = function(coord) {return this.fromPointToLatLng(this.fromCoordToPoint(coord))};
-
-myProjection.prototype.fromLatLngToPoint = function(latLng) {
-  var origin = this.worldOrigin_;
-  var x = origin.x + latLng.lng() * this.pixelsPerLonDegree_;
-  var y = origin.y + latLng.lat() * this.pixelsPerLatDegree_;
-  return new google.maps.Point(x, y);
-};
-
-myProjection.prototype.fromPointToLatLng = function(point) {
-  var origin = this.worldOrigin_;
-  var lng = (point.x - origin.x) / this.pixelsPerLonDegree_;
-  var lat = (point.y - origin.y) / this.pixelsPerLatDegree_;
-  return new google.maps.LatLng(lat, lng);
-};
-
-CoordMapType.prototype.getTile = function(coord, zoom, ownerDocument) {
-  var factor = Math.pow(2,9-zoom);
-  var x = ((coord.x)*factor);
-  var y = ((coord.y)*factor);
-  var div = ownerDocument.createElement('div');
-  div.innerHTML = '('+x+','+y+')';
-  div.style.width = this.tileSize.width + 'px';
-  div.style.height = this.tileSize.height + 'px';
-  div.style.fontSize = '10';
-  div.style.borderStyle = 'solid';
-  div.style.borderWidth = '1px';
-  div.style.borderColor = '#000000';
-  div.className = 'gridTile';
-  return div;
-};
-
-CoordMapType.prototype.toggle = function() {
-  if (map.overlayMapTypes.getLength()) {
-    map.overlayMapTypes.pop();
-  } else {
-    map.overlayMapTypes.push(new CoordMapType(new google.maps.Size(tsz[0], tsz[1])))
-  }
-};
 
 var overlay = new CoordMapType(new google.maps.Size(tsz[0], tsz[1]));
 
@@ -72,6 +27,7 @@ var map = new google.maps.Map(document.getElementById('map'), {
 
 var PublicMap = new google.maps.ImageMapType({
   getTileUrl: function(coord, zoom) {
+      coord = fixCoord(coord, zoom);
       var x = coord.x;
       var y = coord.y;
       return 'http://www.odditown.com/haven/map/tiles/'+zoom+'/'+x+'_'+y+'.png';
@@ -84,6 +40,7 @@ var PublicMap = new google.maps.ImageMapType({
 
 var LandMap = new google.maps.ImageMapType({
   getTileUrl: function(coord, zoom) {
+      coord = fixCoord(coord, zoom);
       var x = coord.x;
       var y = coord.y;
       return tileset+'land/'+zoom+'/tile_'+x+'_'+y+'.png';
@@ -96,6 +53,7 @@ var LandMap = new google.maps.ImageMapType({
 
 var Cave1Map = new google.maps.ImageMapType({
   getTileUrl: function(coord, zoom) {
+      coord = fixCoord(coord, zoom);
       var x = coord.x;
       var y = coord.y;
       return tileset+'cave1/'+zoom+'/tile_'+x+'_'+y+'.png';
@@ -108,6 +66,7 @@ var Cave1Map = new google.maps.ImageMapType({
 
 var Cave2Map = new google.maps.ImageMapType({
   getTileUrl: function(coord, zoom) {
+      coord = fixCoord(coord, zoom);
       var x = coord.x;
       var y = coord.y;
       return tileset+'cave2/'+zoom+'/tile_'+x+'_'+y+'.png';
@@ -119,6 +78,7 @@ var Cave2Map = new google.maps.ImageMapType({
 });
 var Cave3Map = new google.maps.ImageMapType({
   getTileUrl: function(coord, zoom) {
+      coord = fixCoord(coord, zoom);
       var x = coord.x;
       var y = coord.y;
       return tileset+'cave3/'+zoom+'/tile_'+x+'_'+y+'.png';
@@ -130,6 +90,7 @@ var Cave3Map = new google.maps.ImageMapType({
 });
 var Cave4Map = new google.maps.ImageMapType({
   getTileUrl: function(coord, zoom) {
+      coord = fixCoord(coord, zoom);
       var x = coord.x;
       var y = coord.y;
       return tileset+'cave4/'+zoom+'/tile_'+x+'_'+y+'.png';
@@ -141,6 +102,7 @@ var Cave4Map = new google.maps.ImageMapType({
 });
 var Cave5Map = new google.maps.ImageMapType({
   getTileUrl: function(coord, zoom) {
+      coord = fixCoord(coord, zoom);
       var x = coord.x;
       var y = coord.y;
       return tileset+'cave5/'+zoom+'/tile_'+x+'_'+y+'.png';
@@ -151,6 +113,7 @@ var Cave5Map = new google.maps.ImageMapType({
   name: 'Cave 5'
 });
 
+var projection;
 projection = new myProjection();
 PublicMap.projection = projection
 LandMap.projection = projection
